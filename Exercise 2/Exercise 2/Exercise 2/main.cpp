19cb864d1e37841635f96b87357dcee3b5e1fa18
Velitchko Filipov <velitchko@crocotta.co.uk>
2015-05-19 15:24:31 +0200

    select disparity impl

--- Exercise 2/Exercise 2/Exercise 2/main.cpp
+++ Exercise 2/Exercise 2/Exercise 2/main.cpp
@@ -24,24 +24,27 @@ int main(int argc, char* argv[])
 	cv::Mat imgGray_l(img_left.rows, img_left.cols, CV_8UC1, cv::Scalar(0));
 	cv::Mat imgGray_r(img_right.rows, img_right.cols, CV_8UC1, cv::Scalar(0));
 
-	cv::Mat disp_left(img_left.rows, img_left.cols, CV_32FC1, cv::Scalar(0));
-	cv::Mat disp_right(img_right.rows, img_right.cols, CV_32FC1, cv::Scalar(0));
+	cv::Mat disp_left(img_left.rows, img_left.cols, CV_8UC1, cv::Scalar(0));
+	cv::Mat disp_right(img_right.rows, img_right.cols, CV_8UC1, cv::Scalar(0));
 	
 	convertToGrayscale(img_left, imgGray_l);
 	convertToGrayscale(img_right, imgGray_r);
 
 	int maxDisp = 15;
-	int windowSize = 3;
+	int windowSize = 5;
 	int scaleFactor = 16;
 
 	std::vector<cv::Mat> *costVolumeLeft = new std::vector<cv::Mat>(maxDisp);
 	std::vector<cv::Mat> *costVolumeRight = new std::vector<cv::Mat>(maxDisp);
 
 	computeCostVolume(imgGray_l, imgGray_r, *costVolumeLeft, *costVolumeRight, windowSize, maxDisp);
-	show(*costVolumeLeft, "Left", maxDisp);
+	//show(*costVolumeLeft, "Left", maxDisp);
+	//show(*costVolumeRight, "Right", maxDisp);
 	
 	selectDisparity(disp_left, disp_right, *costVolumeLeft, *costVolumeRight, scaleFactor);
 
+	cv::imshow("Disparity Left", disp_left);
+	cv::imshow("Disparity Right", disp_right);
 	
 	//cv::imshow("Gray Left", imgGray_l);
 	//cv::imshow("Gray Right", imgGray_r);
@@ -51,6 +54,7 @@ int main(int argc, char* argv[])
 	return 0;
 }
 
+// Visualizes Cost Volume Matrices for each disparity value
 void show(const std::vector<cv::Mat> &costVolume, const std::string left_right, int maxDisp)
 {
 
@@ -61,6 +65,7 @@ void show(const std::vector<cv::Mat> &costVolume, const std::string left_right, 
 	}
 }
 
+// Converts images to grayscale CV_8UC1
 void convertToGrayscale(const cv::Mat &img, cv::Mat &imgGray)
 {
 	for(int i = 0; i < imgGray.rows; i++)
@@ -129,11 +134,7 @@ void computeCostVolume(const cv::Mat &imgLeft, const cv::Mat &imgRight, std::vec
 						sad_right += abs(gray_right_r - gray_left_r);
 					}
 				}
-				float sad = (float) sad_left / (255.0f*windowSize*windowSize);
-				if(sad > 1.0)
-				{
-					std::cout << "SAD: " << sad << std::endl;
-				}
+				
 				costVolumeLeft.at(d).at<float>(i,j) = (float) sad_left / 255.0f;
 				costVolumeRight.at(d).at<float>(i,j) = (float) sad_right / 255.0f;
 			}
@@ -145,5 +146,53 @@ void computeCostVolume(const cv::Mat &imgLeft, const cv::Mat &imgRight, std::vec
 // scaleDispFactor of 16 for a maxDisp value of 15
 void selectDisparity(cv::Mat &dispLeft, cv::Mat &dispRight, std::vector<cv::Mat> &costVolumeLeft, std::vector<cv::Mat> &costVolumeRight, int scaleDispFactor)
 {
+
+	// go through all disparities
+	int width = dispLeft.cols;
+	int height = dispLeft.rows;
+	int disp_left = 0;
+	int disp_right = 0;
+	// Y
+	for(int i = 0; i < height; i++)
+	{
+		// X
+		for(int j = 0; j < width; j++)
+		{
+			float min_cost_value_l = FLT_MAX;
+			float min_cost_value_r = FLT_MAX;
+			// Disparities
+			for(int d = 0; d < costVolumeLeft.size(); d++)
+			{
+				cv::Mat cvl = costVolumeLeft.at(d);
+				cv::Mat cvr = costVolumeRight.at(d);
+
+				float cvl_cost_value = cvl.at<float>(i,j);
+				float cvr_cost_value = cvr.at<float>(i,j);
+
+				// find pixel with lowest cost value
+				// set dispLeft / Right to the disparity of that pixel
+				if(cvl_cost_value < min_cost_value_l)
+				{
+					min_cost_value_l = cvl_cost_value;
+					disp_left = d;
+					//std::cout << "new value for left: " << d << std::endl;
+				}
+
+				if(cvr_cost_value < min_cost_value_r)
+				{
+					min_cost_value_r = cvr_cost_value;
+					disp_right = d;
+					//std::cout << "new value for right: " << d << std::endl;
+				}
+			}
+
+			if(disp_left*scaleDispFactor > 255 || disp_right*scaleDispFactor > 255)
+			{
+				std::cout << "Disparity above 255" << std::endl;
+			}
+			dispLeft.at<uchar>(i,j) = disp_left*scaleDispFactor;
+			dispRight.at<uchar>(i,j) = disp_right*scaleDispFactor;
+		}
+	}
 }
 
